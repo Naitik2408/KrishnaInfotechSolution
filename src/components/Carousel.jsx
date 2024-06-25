316526c4390c58402033598431e607e11f9a263b
Naitik2408 <naitikkumar2408@gmail.com>
2024-06-25 18:52:18 +0530

    test 2

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -17,7 +17,7 @@ const Carousel = ({ images, texts, subTexts }) => {
 
   // const whatsAppUrl = `https://wa.me/8582937283?text=Hello,%20I'm%20interested%20in%20your%20services.`;
 
-  const whatsAppUrl = `https://wa.me/9060557296?text=Hello,%20I'm%20interested%20in%20your%20service.`;
+  const whatsAppUrl = `https://wa.me/9875306855?text=Hello,%20I'm%20interested%20in%20your%20service.`;
 
   return (
     <section id="homeSection" className="w-full overflow-hidden relative">
